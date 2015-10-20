7448f5e4c4c1804b66e034b324ce05b96de9982d
mslatour <sanderlatour@gmail.com>
2015-10-21 00:05:57 +0200

    Introduced CourseGroups

--- course/migrations/0001_initial.py
+++ course/migrations/0001_initial.py
@@ -17,8 +17,18 @@ class Migration(migrations.Migration):
                 ('url', models.URLField(max_length=255)),
                 ('title', models.CharField(max_length=255)),
                 ('active', models.BooleanField(default=True)),
-                ('start_date', models.DateField()),
                 ('last_updated', models.DateTimeField(null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='CourseGroup',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('start_date', models.DateField()),
+                ('end_date', models.DateField()),
+                ('name', models.CharField(unique=True, max_length=50)),
+                ('label', models.CharField(max_length=255, blank=True)),
+                ('course', models.ForeignKey(to='course.Course')),
             ],
         ),
         migrations.CreateModel(
@@ -28,5 +38,10 @@ class Migration(migrations.Migration):
                 ('label', models.CharField(max_length=255)),
                 ('identification', models.CharField(max_length=255)),
             ],
+        ),
+        migrations.AddField(
+            model_name='coursegroup',
+            name='members',
+            field=models.ManyToManyField(related_name='statistic_groups', to='course.Student', blank=True),
         ),
     ]
--- course/models.py
+++ course/models.py
@@ -18,7 +18,6 @@ class Course(models.Model):
     url = models.URLField(max_length=255)
     title = models.CharField(max_length=255)
     active = models.BooleanField(default=True, blank=True)
-    start_date = models.DateField()
     last_updated = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
@@ -29,4 +28,30 @@ class Course(models.Model):
 
     def __repr__(self):
         return "Course(%s)" % (self.url,)
+
+class CourseGroup(models.Model):
+    course = models.ForeignKey('Course')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    name = models.CharField(max_length=50, unique=True)
+    label = models.CharField(max_length=255, blank=True)
+    members = models.ManyToManyField('course.Student', blank=True,
+            related_name='statistic_groups')
+
+    @classmethod
+    def get_groups_by_date(cls, date, **kwargs):
+        return cls.objects.filter(start_date__lte=date,
+                end_date__gte=date, **kwargs)
+
+    def natural_key(self):
+        return (self.name,)
+
+    def __unicode__(self):
+        return unicode(self.label or self.name)
+
+    def __str__(self):
+        return unicode(self).encode('ascii', 'xmlcharrefreplace')
+
+    def __repr__(self):
+        return "CourseGroup(%s::%s)" % (self.course.title, self.name)
 
