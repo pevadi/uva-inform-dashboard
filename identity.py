ff9132af75d92620b882f836881305105c43b754
mslatour <sanderlatour@gmail.com>
2015-10-22 19:19:16 +0200

    Added support for both version of URL signing.

--- identity.py
+++ identity.py
@@ -1,33 +1,42 @@
 from django.http import HttpResponseBadRequest
 from django.conf import settings
 
-from hashlib import sha256
+from hashlib import sha256, md5
+from urllib import quote
 
 def identity_required(func):
     def inner(request, *args, **kwargs):
-        # Fetch email from GET paramaters if present and store in session.
         user = request.GET.get('user', None)
-        course = request.GET.get('course', None)
-        timestamp = request.GET.get('timestamp', None)
         param_hash = request.GET.get('hash', None)
-        if param_hash is not None:
-            secret = settings.AUTHENTICATION_SECRET
-            hash_contents = [user, timestamp, course, secret]
-            hash_string = sha256(",".join(hash_contents)).hexdigest().upper()
-            if hash_string == param_hash.upper() and user is not None and user != "":
-                request.session['user'] = user
-                request.session['course'] = course
+        course = request.GET.get('course', None)
+        rich_url = request.GET.get('UrlDynamicCreatedBy', None) is not None
+        secret = settings.AUTHENTICATION_SECRET
 
-        # Fetch user from session
-        user = request.session.get('user', None)
+        if user and course and param_hash:
+            if rich_url:
+                paramlist = request.GET.get('paramlist', "")
+                hash_contents = []
+                for param in paramlist.split(","):
+                    if param == "pw":
+                        hash_contents.append(secret)
+                    else:
+                        hash_contents.append(request.GET.get(param, ""))
+                hash_string = (md5(",".join(hash_contents)).
+                         hexdigest().upper())
+            else:
+                timestamp = request.GET.get('timestamp', None)
+                hash_contents = [user, timestamp, course, secret]
+                hash_string = (sha256(",".join(hash_contents)).
+                        hexdigest().upper())
 
-        if user is None:
-            return HttpResponseBadRequest()
-        else:
-            request.signed_url_params = generate_signed_params(
-                request.session.get("user"),
-                request.session.get("course"))
-            return func(request, *args, **kwargs)
+            if hash_string == param_hash.upper():
+                request.signed_url_params = generate_signed_params(
+                    user, course)
+                request.authenticated_course = course
+                request.authenticated_user = user
+                return func(request, *args, **kwargs)
+            else:
+                return HttpResponseBadRequest()
     return inner
 
 def generate_signed_params(user, course, timestamp=None):
@@ -35,7 +44,7 @@ def generate_signed_params(user, course, timestamp=None):
     from urllib import quote
     timestamp = timestamp or str(int(time()))
     secret = settings.AUTHENTICATION_SECRET
-    hash_string = (sha256(",".join([user,str(timestamp), course, secret])).
+    hash_string = (sha256(",".join([user, str(timestamp), course, secret])).
             hexdigest().upper())
     return "user=%s&timestamp=%s&course=%s&hash=%s" % (
             user, timestamp, quote(course, ''), hash_string)
--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.decorators.clickjacking import xframe_options_exempt
 
+from identity import identity_required
+
 @xframe_options_exempt
+@identity_required
 def render_dashboard(request):
     return render(request, "dashboard.html", {});
