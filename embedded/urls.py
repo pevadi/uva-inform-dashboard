af1b7c1746cc4e7c33482537c85009a536a31530
mslatour <sanderlatour@gmail.com>
2015-10-22 15:29:05 +0200

    Added empty framed loader for embedding in iframes

--- embedded/urls.py
+++ embedded/urls.py
@@ -3,6 +3,7 @@ from views import *
 
 urlpatterns = [
     url(r'^bootstrap/?$', bootstrap, name='bootstrap'),
+    url(r'^framed/?$', framed, name='framed'),
     url(r'^scripts/ping/?$', get_ping_script, name='ping_script'),
     url(r'^scripts/video/?$', get_video_script, name='video_script'),
     url(r'^scripts/dashboard/?$', get_dashboard_script,
--- embedded/views.py
+++ embedded/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
+
 from identity import identity_required
 
 @identity_required
@@ -6,6 +9,10 @@ def bootstrap(request):
     return render(request, "js/bootstrap.js",
         { "user": request.session.get("user")},
         content_type="application/javascript")
+
+@xframe_options_exempt
+def framed(request):
+    return render(request, "frame_loader.html")
 
 @identity_required
 def get_ping_script(request):
