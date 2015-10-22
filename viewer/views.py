5ac1a8f6f2a039298ec3140a953112c95aff7dc6
mslatour <sanderlatour@gmail.com>
2015-10-22 15:11:50 +0200

    Added IFRAME-ORIGIN exempt for dashboard.

--- viewer/views.py
+++ viewer/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
+@xframe_options_exempt
 def render_dashboard(request):
     return render(request, "dashboard.html", {});
