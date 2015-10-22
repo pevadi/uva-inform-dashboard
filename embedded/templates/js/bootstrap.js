834551166ebc4879d83e2691accb14127e40e8fe
mslatour <sanderlatour@gmail.com>
2015-10-22 15:07:42 +0200

    Ensured verification is performed after page load.

--- embedded/templates/js/bootstrap.js
+++ embedded/templates/js/bootstrap.js
@@ -2,10 +2,12 @@
 // Ensure jQuery is loaded
 window.jQuery || document.write('<script src="//{{ request.get_host }}{% static "js/jquery-1.11.3.min.js" %}">\x3C/script>');
 
-$("<div>").addClass("alert alert-success alert-dismissible")
+$(function(){
+	$("<div>").addClass("alert alert-success alert-dismissible")
 	.text("User {{ user }} has been successfully authenticated for the dashboard.")
 	.append($("<button type='button' class='close' data-dismiss='alert'>×</button>"))
 	.insertAfter(".navbar")
+})
 
 $.getScript("//{{ request.get_host }}{% url 'embed:ping_script' %}?{{ request.signed_url_params|safe }}")
 
