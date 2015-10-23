c71b92cb0c7f6752e2fcc7fe824465760434b140
mslatour <sanderlatour@gmail.com>
2015-10-23 17:11:35 +0200

    Remove deprecated prediction models and views

--- stats/urls.py
+++ stats/urls.py
@@ -4,8 +4,6 @@ from views import *
 urlpatterns = [
     url(r'^prediction/(?P<bin_pk>[^/]+)/(?P<variable_name>[^/]+)/',
         get_variable_prediction, name='prediction'),
-    url(r'^prediction_table/(?P<output_label>[^/]+)/', get_prediction_table,
-        name='prediction_table'),
     url(r'^(?P<variable_name>[^/]+)/', get_variable_stats,
         name='variable_stats'),
 ]
