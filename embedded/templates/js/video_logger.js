efbd778b990ceb28996b2320474e62bc80b8ff69
mslatour <sanderlatour@gmail.com>
2015-10-25 00:09:35 +0200

    Fixed URL reference

--- embedded/templates/js/video_logger.js
+++ embedded/templates/js/video_logger.js
@@ -52,7 +52,8 @@ YTVideo = function(videoId, height, width, containerId){
 			_this.debug("Counting seconds: "+ secondsWatched);
 			_this.last_time_tic = current_time;
 			_this.timer = setTimeout(_this.onTimeWatched, timeoutInterval);
-	        $.post("//{{ request.get_host }}{% url 'store_video_watch_event' %}?{{ request.signed_url_params|safe }}",{
+			var signed_params = "{{ request.signed_url_params|safe }}";
+	        $.post("//{{ request.get_host }}{% url 'storage:store_video_watch_event' %}?"+signed_params,{
                 "video": _this.player.getVideoUrl(),
                 "duration": Math.round(secondsWatched*100)/100
             });
