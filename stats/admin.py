5d7e485ac2908dc11369bb51ec3d05d16a6836ef
mslatour <sanderlatour@gmail.com>
2015-10-24 04:32:10 +0200

    Allowed filtering valuehistory on course.

--- stats/admin.py
+++ stats/admin.py
@@ -4,6 +4,7 @@ from .models import *
 class ValueHistoryAdmin(admin.ModelAdmin):
     list_display = ('student', 'group', 'variable', 'value', 'course_datetime',
             'datetime')
+    list_filter = ('group__course',)
 
 admin.site.register(AveragingVariable)
 admin.site.register(ValueHistory, ValueHistoryAdmin)
