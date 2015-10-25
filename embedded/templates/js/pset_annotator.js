166523dc4dd623c32f9ab059b6ff35309b1bc653
mslatour <sanderlatour@gmail.com>
2015-10-25 17:58:40 +0100

    Fixed link annotator

--- embedded/templates/js/pset_annotator.js
+++ embedded/templates/js/pset_annotator.js
@@ -1,8 +1,8 @@
 $(function(){
 	$("a").filter(function(index){
-		var href = this.attr("href");
-		if(href.match(/^http://cdn.mprog.nl/prog-ai/pset[0-9]+.zip/)){
-			this.attr(href, "{{ request.get_host }}{% url 'get_zip' %}?{{ request.signed_url_params|safe }}&f="+encodeURIComponent(href))
+		var href = $(this).attr("href");
+		if(href && href.match(/^http:\/\/cdn.mprog.nl\/prog-ai\/pset[0-9]+\.zip/)){
+			$(this).attr('href', "{{ request.get_host }}{% url 'get_zip' %}?{{ request.signed_url_params|safe }}&f="+encodeURIComponent(href))
 		}
 	})
 })
