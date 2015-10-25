8189a7f18c71d01babb4d7f7b1950d4a6fed9b0b
mslatour <sanderlatour@gmail.com>
2015-10-25 18:07:02 +0100

    Fixed http protocol in pset link annotator.

--- embedded/templates/js/pset_annotator.js
+++ embedded/templates/js/pset_annotator.js
@@ -1,8 +1,9 @@
 $(function(){
+	var base_url = "{% if request.is_secure %}https{% else %}http{% endif %}://{{ request.get_host }}{% url 'get_zip' %}?{{ request.signed_url_params|safe }}&f=";
 	$("a").filter(function(index){
 		var href = $(this).attr("href");
 		if(href && href.match(/^http:\/\/cdn.mprog.nl\/prog-ai\/pset[0-9]+\.zip/)){
-			$(this).attr('href', "{{ request.get_host }}{% url 'get_zip' %}?{{ request.signed_url_params|safe }}&f="+encodeURIComponent(href))
+			$(this).attr('href', base_url+encodeURIComponent(href))
 		}
 	})
 })
