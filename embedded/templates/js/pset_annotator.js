dfba81237337b6db83789b22429f2a3f36b492d0
mslatour <sanderlatour@gmail.com>
2015-10-26 12:12:45 +0100

    fixed regex

--- embedded/templates/js/pset_annotator.js
+++ embedded/templates/js/pset_annotator.js
@@ -2,7 +2,7 @@ $(function(){
 	var base_url = "{% if request.is_secure %}https{% else %}http{% endif %}://{{ request.get_host }}{% url 'get_zip' %}?{{ request.signed_url_params_unquoted|safe }}&f=";
 	$("a").filter(function(index){
 		var href = $(this).attr("href");
-		if(href && href.match(/^http:\/\/cdn.mprog.nl\/prog-ai\/pset[0-9]+\.zip/)){
+		if(href && href.match(/^http:\/\/cdn.mprog.nl\/prog-ai-new\/pset[0-9]+\.zip/)){
 			$(this).attr('href', base_url+encodeURIComponent(href))
 		}
 	})
