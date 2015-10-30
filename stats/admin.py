fc719a8af90b8e188d6852a0d1680efa4616fc73
mslatour <sanderlatour@gmail.com>
2015-10-30 13:39:40 +0100

    Added ignored objects to admin.

--- stats/admin.py
+++ stats/admin.py
@@ -25,3 +25,4 @@ class VariableAdmin(PolymorphicParentModelAdmin):
 
 admin.site.register(Variable, VariableAdmin)
 admin.site.register(ValueHistory, ValueHistoryAdmin)
+admin.site.register(IgnoredObject)
