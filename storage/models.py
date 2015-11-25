efb911408b18bd1a05604c0167ff43158e487ffa
mslatour <sanderlatour@gmail.com>
2015-11-25 13:03:45 +0100

    Updated identitiy check to include extensions.

--- storage/models.py
+++ storage/models.py
@@ -133,13 +133,23 @@ class Activity(models.Model):
         else:
             course = None
 
-        activity, created = cls.objects.get_or_create(user=user, verb=verb,
+        for activity in cls.objects.filter(user=user, verb=verb,
                 course=course, activity=activity, time=time,
                 type=statement_type, value=value, name=name,
                 value_min=min_score, value_max=max_score,
-                description=description, defaults={"remotely_stored": stored})
-        for extension in extensions:
-            activity.extensions.add(extension)
+                description=description):
+            if set(activity.extensions.all()) == set(extensions):
+                created = False
+                break
+        else:
+            created = True
+            activity = cls.objects.create(user=user, verb=verb,
+                    course=course, activity=activity, time=time,
+                    type=statement_type, value=value, name=name,
+                    value_min=min_score, value_max=max_score,
+                    description=description, remotely_stored=stored)
+            for extension in extensions:
+                activity.extensions.add(extension)
 
         return activity, created
 
