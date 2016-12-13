46a714570777b12435c8d24774a82af696f315ae
Pepijn van Diepen <pepijnvandiepen@gmail.com>
2016-12-13 12:30:32 +0100

    Added instructor authentication such that the application can differentiate between students and instructors and display different dashboards.

--- viewer/views.py
+++ viewer/views.py
@@ -8,8 +8,12 @@ from .models import GroupAssignment
 from stats.models import ValueHistory, CourseGroup, Student
 from storage.helpers import *
 from course.models import Course
+from storage.models import IgnoredUser
 from datetime import date, timedelta
 import logging
+from django.contrib.auth.models import User
+import operator
+
 
 @xframe_options_exempt
 @identity_required
@@ -19,11 +23,10 @@ def render_dashboard(request):
     course = get_object_or_404(Course, url=request.authenticated_course)
     day_shift = timedelta(days=int(request.GET.get('day_shift', '0')))
     
-    print 'In render_dashboard', course, day_shift, request.authenticated_course 
     if log_access_event:
         DashboardAccessEvent(request.authenticated_user, request.authenticated_course).store()
 
-    # Retrieve current group context
+    # Retrieve student and current group context
     try:
         student = Student.objects.get(identification=request.session.get("authenticated_user"))
     except Student.DoesNotExist:
@@ -43,24 +46,41 @@ def render_dashboard(request):
             'day_shift': request.GET.get('day_shift', '0'),
             'input_variables': [],
         });
- 
-    groups = CourseGroup.get_groups_by_date(date.today()+day_shift, course__url=request.authenticated_course, members=student)
-    if groups:
-        group = groups[0]
+
+
+    # Check if user is instructor
+    if User.objects.filter(username=request.session.get("authenticated_user")):
+        if User.objects.filter(username=request.session.get("authenticated_user"))[0].is_authenticated:
+            students = Student.objects.filter(statistic_groups=group)
+            no_grade_list = []
+            for x in xrange(len(students)):
+                student = students[x]
+                if not student.grade_so_far:
+                    no_grade_list.append(x)
+            for i in sorted(no_grade_list, reverse=True):
+                students = students.exclude(identification=students[i])
+
+            students = sorted(students, key=operator.attrgetter('grade_so_far'))
+             # Determine what variables the first student has data for (and thus will be forwarded to their dropdown menu)
+            variables = course.variable_set.exclude(type='OUT').order_by('order')
+            course_datetime_now = group.calculate_course_datetime(timezone.now()+day_shift)
+            variables = [x for x in variables if not len(ValueHistory.objects.filter(variable=x, course_datetime__lte=course_datetime_now, student=students[0].identification, group=group)) == 0]
+            return render(request, "instructor_dashboard.html", {
+                'students': students,
+                'input_variables':  variables,
+                'day_shift': request.GET.get('day_shift', '0'),
+            });
+    # Student view
     else:
-        print 'Failed to find group with', student, request.authenticated_course
-  
-    # Determine what variables the student has data for (and thus will be forwarded to their dropdown menu)
-    variables = course.variable_set.exclude(type='OUT').order_by('order')
-
-    course_datetime_now = group.calculate_course_datetime(timezone.now()+day_shift)
-    variables = [x for x in variables if not len(ValueHistory.objects.filter(variable=x, course_datetime__lte=course_datetime_now, student=student.identification, group=group)) == 0]
-
-    return render(request, "dashboard.html", {
-        'log_access': log_access_event,
-        'day_shift': request.GET.get('day_shift', '0'),
-        'input_variables': variables,
-    });
+        # Determine what variables the student has data for (and thus will be forwarded to their dropdown menu)
+        variables = course.variable_set.exclude(type='OUT').order_by('order')
+        course_datetime_now = group.calculate_course_datetime(timezone.now()+day_shift)
+        variables = [x for x in variables if not len(ValueHistory.objects.filter(variable=x, course_datetime__lte=course_datetime_now, student=student.identification, group=group)) == 0]
+        return render(request, "dashboard.html", {
+            'log_access': log_access_event,
+            'day_shift': request.GET.get('day_shift', '0'),
+            'input_variables': variables,
+        });
 
 @identity_required
 def has_treatment(request):
