71b165f932e886af64fc649871c458a25975480b
Pepijn van Diepen <pepijnvandiepen@gmail.com>
2016-12-13 12:57:24 +0100

    Some evaluation functionality was added to this file. These functions will be moved to a more appropriate location soon. Also authentication was added to the get stats function in order to increase security.

--- stats/views.py
+++ stats/views.py
@@ -111,24 +111,25 @@ def evaluate_activities(request=None, debug_out=None):
     import csv
     import sys
     from storage.models import Activity
+    from scipy.stats import pearsonr
     np.set_printoptions(threshold=sys.maxint)
 
-    result_matrix = [['Verb','object', 'Correlation', 'Variance']]
+    result_matrix = [['Verb','object', 'Correlation', 'p-value', 'n', 'Variance']]
     # result_matrix_by_week = [['Verb','object', 'before','week 1', 'week 2', 'week 3', 'week 4', 'week 5', 'week 6', 'week 7', 'week 8', 'all time']]
 
     # 2014
-    episode = '2016'
-    group = "{'group': 3}"
+    # episode = '2016'
+    # group = "{'group': 3}"
     # Cummalative
     # time_ranges = [["2014-10-01", "2014-10-27"],["2014-10-01", "2014-11-03"],["2014-10-01", "2014-11-10"], ["2014-10-01", "2014-11-17"],["2014-10-01", "2014-11-24"],["2014-10-01", "2014-12-01"],["2014-10-01", "2014-12-08"],["2014-10-01", "2014-12-15"],["2014-10-01", "2014-12-22"]]
     # Per week
-    time_ranges = [["2014-10-01", "2014-10-27"],["2014-10-27", "2014-11-03"],["2014-11-03", "2014-11-10"], ["2014-11-10", "2014-11-17"],["2014-11-17", "2014-11-24"],["2014-11-24", "2014-12-01"],["2014-12-01", "2014-12-08"],["2014-12-08", "2014-12-15"],["2014-12-15", "2014-12-22"]]
+    # time_ranges = [["2014-10-01", "2014-10-27"],["2014-10-27", "2014-11-03"],["2014-11-03", "2014-11-10"], ["2014-11-10", "2014-11-17"],["2014-11-17", "2014-11-24"],["2014-11-24", "2014-12-01"],["2014-12-01", "2014-12-08"],["2014-12-08", "2014-12-15"],["2014-12-15", "2014-12-22"]]
 
     # 2015
-    # episode = '2015'
-    # group = "{'group': 3}"
+    episode = '2015'
+    group = "{'group': 3}"
     # Cummalative
-    # time_ranges = [["2015-10-01", "2015-10-26"],["2015-10-01", "2015-11-02"],["2015-10-01", "2015-11-09"], ["2015-10-01", "2015-11-16"],["2015-10-01", "2015-11-23"],["2015-10-01", "2015-11-30"],["2015-10-01", "2015-12-07"],["2015-10-01", "2015-12-14"],["2015-10-01", "2015-12-21"]]
+    time_ranges = [["2015-10-01", "2015-10-26"],["2015-10-01", "2015-11-02"],["2015-10-01", "2015-11-09"], ["2015-10-01", "2015-11-16"],["2015-10-01", "2015-11-23"],["2015-10-01", "2015-11-30"],["2015-10-01", "2015-12-07"],["2015-10-01", "2015-12-14"],["2015-10-01", "2015-12-21"]]
     # Per week
     # time_ranges = [["2015-10-01", "2015-10-26"],["2015-10-26", "2015-11-02"],["2015-11-02", "2015-11-09"], ["2015-11-09", "2015-11-16"],["2015-11-16", "2015-11-23"],["2015-11-23", "2015-11-30"],["2015-11-30", "2015-12-07"],["2015-12-07", "2015-12-14"],["2015-12-14", "2015-12-21"]]
 
@@ -136,7 +137,7 @@ def evaluate_activities(request=None, debug_out=None):
 
     # For each episode try to retrieve the final grades from an external file
     try:
-        with open('iki_grades_'+str(group)+'.csv', mode='r') as infile:
+        with open('../../iki_grades_'+str(group)+'.csv', mode='r') as infile:
             reader = csv.reader(infile)
             iki_grades = {rows[0]:rows[1] for rows in reader}
     except IOError:
@@ -160,25 +161,165 @@ def evaluate_activities(request=None, debug_out=None):
             for student_id in student_ids:
                 if student_id not in ignored_users:
                     relevant_activities = Activity.objects.filter(time__year=episode, time__range=time_range, user=student_id, verb=possible_variable['verb'], type=possible_variable['type'])
-                    data_vector.append(len(relevant_activities))
+                    # data_vector.append(len(relevant_activities))
 
                     # Add final grades from external file (if present)
                     try:
                         final_grades.append(iki_grades[student_id])
+                        data_vector.append(len(relevant_activities))
                     except KeyError:
-                        final_grades.append(None)
+                        pass
+                        # final_grades.append(None)
 
             # Determine correlation of n# activity occurences and the final grade
             # Due to the missing values np.ma.corrcoef is to be used. This required some preprocessing of the arrays
             a = np.ma.array(np.array(final_grades, dtype=np.float), mask=np.isnan(np.array(final_grades, dtype=np.float)))
             b = np.ma.array(np.array(data_vector, dtype=np.float), mask=np.isnan(np.array(data_vector, dtype=np.float)))
-            print 'Correlation', np.ma.corrcoef(a,b)[0][1]
-            print 'Variance', np.var(b)
-            result_matrix.append([possible_variable['verb'], possible_variable['type'], np.ma.corrcoef(a,b)[0][1], np.var(b)])
-
+            # print 'Correlation', np.ma.corrcoef(a,b)[0][1]
+            # print 'Correlation', pearsonr(a,b)[0], 'P-value (significance)', pearsonr(a,b)[1]
+            # print 'Variance', np.var(b)
+            # print a
+            # print b
+            print len(a), len(b)
+            result_matrix.append([possible_variable['verb'], possible_variable['type'], pearsonr(a,b)[0], pearsonr(a,b)[1], len(a), np.var(b)])
+            # result_matrix.append([possible_variable['verb'], possible_variable['type'], np.ma.corrcoef(a,b)[0][1], np.var(b)])
+        result_matrix.append([])
     # Save to csv
     csv_ready_matrix =  map(str,result_matrix)
     np.savetxt(episode+"_correlation_analysis_perweek.csv", csv_ready_matrix, delimiter=",", fmt='%s')
+
+
+def evaluate_predictive_model(request=None, debug_out=None):
+    from sklearn.preprocessing import normalize
+    from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error, median_absolute_error, r2_score
+    from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
+    from math import sqrt
+    from datetime import timedelta
+    import csv
+    import numpy as np
+    from sklearn.neighbors import KNeighborsRegressor
+    from sklearn import linear_model
+    from sklearn import svm
+    from sklearn import gaussian_process
+    from sklearn import tree
+    from sklearn import neural_network
+
+
+    result_matrix = [['course date time', 'sample size', 'mean absolute error', 'variance', 'root mean square error', 'variance', 'fail/pass accuracy', 'variance', 'at-risk recall', 'variance','baseline', 'variables']]
+    # Get the variables
+    variables = Variable.objects.filter(type='IN').exclude(label='Access Application Count').exclude(label='Interact Application Count')
+    # var_names = variables.values_list("name", flat=True)
+    # print var_names
+    # Set group
+    group = CourseGroup.objects.get(label='Year 2015 - 2016')
+
+    # Get the y values a.k.a. grades needed for machine learning
+    try:
+        with open("/home/pepijn/data_2015.csv", mode='r') as infile:
+            reader = csv.reader(infile)
+            iki_grades = {rows[0]:rows[1] for rows in reader}
+    except IOError, e:
+        print e
+        iki_grades = {}
+
+    # Set measurement points
+    course_datetimes = []
+    for x in xrange(6,60):
+        course_datetimes.append(timedelta(x))
+    # course_datetimes = [timedelta(7), timedelta(14), timedelta(21), timedelta(28), timedelta(35), timedelta(42), timedelta(49), timedelta(56), timedelta(63)]
+    for course_datetime in course_datetimes:
+        result_record = []
+        result_record.append(course_datetime)
+        print 'Course datetime', course_datetime
+        available_variables = []
+
+        # Collect relevant value statistics for model building. Y 2015-2016 (pk=2) was selected for this purpose
+        var_statistics = {}
+        print 'Variables:'
+        for variable in variables:
+            value_history_1516 = ValueHistory.objects.filter(variable=variable.pk, group=group, course_datetime__lte=course_datetime)
+            if len(value_history_1516) == 0:
+                print 'Valuehistory used for model building not found. Check if variables are correctly stored to valuehistory for the following variable:', variable
+            else:
+                print '\t', variable, course_datetime
+                var_statistics[variable.name] = variable.calculate_statistics_from_values(value_history_1516)
+
+        
+        # Variable value dicts is a dictionary holding another dictionary for each individual variable. 
+        # Eacht variable dictionary holds for each student as key his statistics as value.
+        variable_value_dicts = {}
+        # Loop over all variables
+        for var_name in var_statistics.keys():
+            all_values = []
+            variable_value_dicts[var_name] = {}
+
+            # Loop over individual student's statistics in the variable statistics
+            var_statistic = var_statistics[var_name]
+            for indv_student_statistic in var_statistic:
+                # Add the student statistics to the dict
+                student_id = encrypt_value(indv_student_statistic['student'])
+                variable_value_dicts[var_name][student_id] = indv_student_statistic
+
+        # extract data in matrix form from the value dicts
+        X, Y = extract_xy_values(iki_grades.keys(), var_statistics.keys(), variable_value_dicts, iki_grades)  
+        if len(X) == 0:
+            print 'No training data available'
+            return JsonResponse([], safe=False)
+        X = normalize(X, axis=0, norm='max')
+        print len(X), len(Y)
+
+        result_record.append(len(Y))
+
+        # Use machine learning to create a predictive model
+        Y = np.array(Y).astype(dtype='float')
+        Y_binarized = [y>5.4 for y in Y]
+        X = np.array(X).astype(dtype='float')
+       
+        # Deal with low amounts of data
+        if len(Y) < 10: 
+            regr = KNeighborsRegressor(n_neighbors=1)
+        else:
+            regr = KNeighborsRegressor()
+
+        # regr = linear_model.LinearRegression()
+        # regr = linear_model.Ridge (alpha = .5)
+        # regr = linear_model.Lasso(alpha = 0.1)
+        # regr = linear_model.BayesianRidge()
+        # regr = linear_model.SGDRegressor()
+        # regr = svm.SVR()
+        # regr = svm.NuSVR()
+        # regr = svm.LinearSVR()
+        # regr = gaussian_process.GaussianProcessRegressor()
+        # regr = tree.DecisionTreeRegressor()
+        # regr = neural_network.MLPRegressor()
+
+        # Evaluate the model 
+        mean_abs_error, root_mean_square_error, fail_pass_accuracy, failing_students_recall = stratified_cross_val_score(X, Y, regr, Y_binarized, rounds = 50)
+        result_record.append(mean_abs_error[0])
+        result_record.append(mean_abs_error[1])
+        result_record.append(root_mean_square_error[0])
+        result_record.append(root_mean_square_error[1])
+        result_record.append(fail_pass_accuracy[0])
+        result_record.append(fail_pass_accuracy[1])
+        result_record.append(failing_students_recall[0])
+        result_record.append(failing_students_recall[1])
+
+
+        # Baseline classifiers says always pass
+        baseline_accuracy = (float)(len([x for x in Y if x > 5.4]))/len(Y)
+        result_record.append(baseline_accuracy)
+        print 'Performance report:'
+        print 'Baseline classifier (fail/pass)', baseline_accuracy
+        print 'Mean absolute error', mean_abs_error
+        print 'Root mean square error', root_mean_square_error
+        print 'Fail/pass accuracy', fail_pass_accuracy
+        print 'Failing students recall:',failing_students_recall, '\n' #,'of', n_failing, 'failing students'
+        result_record.append(len(var_statistics.keys()))
+        print len(result_record)
+        result_matrix.append(map(str,result_record))
+    
+    result_matrix = np.array(result_matrix)
+    np.savetxt('../../predictor_analysis/KNN_60.csv', result_matrix, delimiter=',',  fmt='%s')
 
 
 def encrypt_csv(file_name):
@@ -378,6 +519,8 @@ def get_variable_stats(request, variable_names):
     from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
     from math import sqrt
     from datetime import timedelta
+    from django.contrib.auth.models import User
+
     # from storage.models import Activity
     # from course.models import Assignment
     # from django.core.exceptions import ObjectDoesNotExist
@@ -400,10 +543,18 @@ def get_variable_stats(request, variable_names):
         var_labels.append(variables[-1].label[:-6])
 
     # Get student
-    try:
-        student = Student.objects.get(identification=request.session.get("authenticated_user"))
-    except Student.DoesNotExist:
-        return JsonResponse([], safe=False)
+    if request.GET.get('selected_student'):
+        print 'Student selected:', request.GET.get('selected_student')
+        if User.objects.filter(username=request.session.get("authenticated_user"))[0].is_authenticated:
+            try:
+                student = Student.objects.get(identification=request.GET.get('selected_student'))
+            except Student.DoesNotExist:
+                return JsonResponse([], safe=False)
+    else:
+        try:
+            student = Student.objects.get(identification=request.session.get("authenticated_user"))
+        except Student.DoesNotExist:
+            return JsonResponse([], safe=False)
 
     # The day shift parameter allows for admin's to look at the dashboard from
     # the perspective of a different day. Both negative and positive integers
@@ -523,20 +674,18 @@ def get_variable_stats(request, variable_names):
     X_comparison, _ = extract_xy_values(comparison_student_ids, var_names, variable_value_dicts_comparison, {}, x_only=True)
     X_comparison, norms =  normalize(X_comparison, axis=0, norm='max', return_norm=True)
 
-    grade_ranges = [(0,4), (6,7.5), (8.5,10)]
+    grade_ranges = [(0,6), (6,7.5), (8.5,10)]
     segments = []
     for grade_range in grade_ranges:
         temp_seg = [val.identification for val in Student.objects.filter(grade_so_far__range=grade_range)]
         temp_X_seg, _ = extract_xy_values([encrypt_value(val) for val in temp_seg], var_names, variable_value_dicts_comparison, {}, x_only=True)
         temp_X_seg = [normalize_instance(record, norms) for record in temp_X_seg]
-        segments.append(np.mean(temp_X_seg, axis = 0))
-
-
-    print segments
+        if len(temp_X_seg) > 0:
+            segments.append(np.mean(temp_X_seg, axis = 0))
+        else:
+            segments.append([])
 
     mean_statistics = np.mean(X_comparison, axis = 0)
-    # mean_statistics = np.mean(X_comparison_segB, axis = 0)
-    # seg_A_statistics = np.mean(X_comparison_segA, axis = 0)
     student_statistics = normalize_instance(student_statistics, norms)
 
     bin_sizes = []
@@ -569,7 +718,7 @@ def get_variable_stats(request, variable_names):
         regr = KNeighborsRegressor()
 
     # Evaluate the model 
-    mean_abs_error, root_mean_square_error, fail_pass_accuracy, failing_students_recall = stratified_cross_val_score(X, Y, regr, Y_binarized, rounds = 10)
+    mean_abs_error, root_mean_square_error, fail_pass_accuracy, failing_students_recall = stratified_cross_val_score(X, Y, regr, Y_binarized, rounds = 50)
 
     # Baseline classifiers says always pass
     baseline_accuracy = (float)(len([x for x in Y if x > 5.4]))/len(Y)
@@ -585,32 +734,23 @@ def get_variable_stats(request, variable_names):
 
     # Get the already obtained grades from the activity db
     total_weight, grade_so_far = get_grade_so_far(student.identification)
-    # assignments = Assignment.objects.all()
-    # total_weight = float(0)
-    # grade_so_far = 0    
-    # for assignment in assignments:
-    #     # Get highest grade assigned (in order to filter out zero values and older grades. Assumes the highest grade is the latest.)
-    #     try:
-    #         assignment_activity =  Activity.objects.filter(user=student.identification, activity=assignment.url).latest('value')
-    #         print assignment, assignment_activity.value, assignment.max_grade, assignment.weight
-    #         if assignment_activity.value != None:
-    #             total_weight += assignment.weight
-    #             grade_so_far += ((assignment_activity.value / assignment.max_grade * 10) * assignment.weight)
-    #     except ObjectDoesNotExist:
-    #         continue
+
     print total_weight, grade_so_far
 
-#grades_so_far = []
-    #print 'grades heritant'
-    #for tm in set(grades_so_far):
-     #   print tm
 
     try:
         print 'Student info:'
         print student.identification, iki_grades[encrypt_value(student.identification)], regr.predict([student_statistics])[0]
     except KeyError:
+        if grade_so_far == None:
+            grade_so_far = 0
         print student.identification, 'no grade', regr.predict([student_statistics])[0], grade_so_far + (regr.predict([student_statistics])[0]*(float(1)-total_weight))
 
+    # Update predicted grade in database
+    if len(student_statistics) > 5:
+        student.predicted_grade = regr.predict([student_statistics])[0]
+        student.assignments_completion = total_weight
+        student.save()
 
     ######################################################################################
     # In case of a single value a bar plot if given
@@ -634,8 +774,10 @@ def get_variable_stats(request, variable_names):
 
             # Normalize mean bin following the max method from sklearn
             mean_bin=(float)(mean_bin - min_values[0])/(max_values[0]-min_values[0])
-            predicted_grade = grade_so_far + (regr.predict([[mean_bin]])[0]*(float(1)-total_weight))
-            print 'met so far', predicted_grade
+            if grade_so_far:
+                predicted_grade = grade_so_far + (regr.predict([[mean_bin]])[0]*(float(1)-total_weight))
+            else:
+                predicted_grade = student.predicted_grade
             predictions['final_grade'] = {'mean': predicted_grade, 'variance':root_mean_square_error[0]}
             predictions['final_grade']['chart'] = 'GSS'
             predictions['final_grade']['label'] = 'Final Grade'
@@ -659,7 +801,10 @@ def get_variable_stats(request, variable_names):
 
     # Otherwise a radar diagram
     elif len(max_values) > 1:
-        predicted_grade = grade_so_far + (regr.predict([student_statistics])[0]*(float(1)-total_weight))
+        if grade_so_far:
+            predicted_grade = grade_so_far + (regr.predict([student_statistics])[0]*(float(1)-total_weight))
+        else:
+            predicted_grade = student.predicted_grade
         prediction = {}
         prediction['final_grade'] = {'mean': predicted_grade, 'variance':root_mean_square_error[0]}
         prediction['final_grade']['chart'] = 'GSS'
