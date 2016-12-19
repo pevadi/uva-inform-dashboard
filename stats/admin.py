6c95421156388a3a17b0c6e16f89010a9d16c3ab
Pepijn van Diepen <pepijnvandiepen@gmail.com>
2016-12-19 16:19:50 +0100

    Meets PEP guidelines now

--- stats/admin.py
+++ stats/admin.py
@@ -1,20 +1,28 @@
+"""
+UID Admin page configuration
+"""
 from django.contrib import admin
-from .models import *
+from .models import SingleEventVariable, AssignmentLinkedVariable,\
+        Variable, ValueHistory, IgnoredObject
 from polymorphic.admin import PolymorphicParentModelAdmin,\
         PolymorphicChildModelAdmin
 
 class ValueHistoryAdmin(admin.ModelAdmin):
+    """ValueHistory admin view"""
     list_display = ('student', 'group', 'variable', 'value', 'course_datetime',
             'datetime')
-    list_filter = ('group','variable__name', 'student')
+    list_filter = ('group', 'variable__name', 'student')
 
 class SingleEventVariableAdmin(PolymorphicChildModelAdmin):
+    """SingleEventVariable Admin view"""
     base_model = SingleEventVariable
 
 class AssignmentLinkedVariableAdmin(PolymorphicChildModelAdmin):
+    """AssignmentLinkedVariable Admin view"""
     base_model = AssignmentLinkedVariable
 
 class VariableAdmin(PolymorphicParentModelAdmin):
+    """Variable Admin view"""
     list_display = ('name', 'label', 'course')
     list_filter = ('type',)
     base_model = Variable
