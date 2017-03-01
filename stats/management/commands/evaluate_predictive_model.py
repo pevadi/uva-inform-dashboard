24d8df29cd798ec91f68c4ca880f07ca49001ae7
Pepijn van Diepen <pepijnvandiepen@gmail.com>
2017-03-01 14:51:01 +0100

    Added a parameter option for this function.

--- stats/management/commands/evaluate_predictive_model.py
+++ stats/management/commands/evaluate_predictive_model.py
@@ -2,7 +2,14 @@ from django.core.management.base import BaseCommand, CommandError
 from stats.helpers import evaluate_predictive_model
 
 class Command(BaseCommand):
-    help = 'Evaluates the predictive model.'
+    help = 'Evaluates the predictive model for a specific coursegroup. Takes a course group name as an argument.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('course_group_name', type=str)
 
     def handle(self, *args, **options):
-        evaluate_predictive_model(None, self.stdout)
+        if options['course_group_name']:
+            course_group_name = options['course_group_name']
+            evaluate_predictive_model(None, self.stdout, course_group_name=course_group_name)
+        else:
+            evaluate_predictive_model(None, self.stdout, None)
