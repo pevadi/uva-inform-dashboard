09a575821343e74f2dea0acee1a696c0472e417c
Pepijn van Diepen <pepijnvandiepen@gmail.com>
2017-03-01 15:05:13 +0100

    added the option to specify a calendar year for the function update_all.

--- storage/management/commands/update_all.py
+++ storage/management/commands/update_all.py
@@ -2,7 +2,14 @@ from django.core.management.base import BaseCommand, CommandError
 from storage.views import update_all
 
 class Command(BaseCommand):
-    help = 'Updates all the activities and variables (including all last years data)'
+    help = 'Updates all the activities and variables from a specified (calendar) year.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('year', type=int)
 
     def handle(self, *args, **options):
-        update_all(None, self.stdout)
+        if options['year']:
+            year = options['year']
+            update_all(None, self.stdout, year=year)
+        else:
+            update_all(None, self.stdout)
